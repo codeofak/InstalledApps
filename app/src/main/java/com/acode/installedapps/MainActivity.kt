6b0e5cceec11106157a840a7f5a3f176ba6103e9
Abdul Kavi <abdkavi.github@gmail.com>
2022-07-20 17:02:33 +0530

    Added ViewModel

--- app/src/main/java/com/acode/installedapps/MainActivity.kt
+++ app/src/main/java/com/acode/installedapps/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,9 +18,11 @@ import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.acode.installedapps.ui.theme.InstalledAppsTheme
+import com.acode.installedapps.viewModels.ViewModelApps
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
@@ -31,6 +34,8 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
 
+                    val viewModelApps: ViewModelApps by viewModels()
+
                     val context = LocalContext.current
 
                     val listApps = installedApps()
@@ -40,29 +45,31 @@ class MainActivity : ComponentActivity() {
                     val listLabel: MutableList<String> = mutableListOf()
 
                     //Method 3 ----Working Good
-                    val listAppsC = listAppsC(context)
+                    //val listAppsC = listAppsC(context)
 
                     listApps.forEach {
                         listLabel += packageManager.getApplicationLabel(it).toString()
                     }
 
 
-                    LazyColumn {
-                        items(listAppsC) {
-                            //for listOfApps
-                            //Text(text = it.applicationInfo.loadLabel(packageManager).toString())
-
-                            // for InstalledAppsA()
-                            //Text(text = it.activityInfo.loadLabel(packageManager).toString())
-
-                            //for listAppsC
-                            Text(text = it)
-
-                            // for listOfAppsB
-                            //Text(text = it)
-                            Divider()
-                        }
-                    }
+//                    LazyColumn {
+//                        items(listAppsB) {
+//                            //for listOfApps
+//                            //Text(text = it.applicationInfo.loadLabel(packageManager).toString())
+//
+//                            // for InstalledAppsA()
+//                            //Text(text = it.activityInfo.loadLabel(packageManager).toString())
+//
+//                            //for listAppsC
+//                            //Text(text = it)
+//
+//                            // for listOfAppsB
+//                            //Text(text = it)
+//                            Divider()
+//                        }
+//                    }
+                    
+                    ListOfApps(context = context, viewModelApps = viewModelApps)
 
                 }
             }
@@ -97,22 +104,34 @@ class MainActivity : ComponentActivity() {
     }
 
     //MEtHOD: 3 ---- Working good
-    fun listAppsC(context: Context): List<String> {
-        val appInfoList = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+//    fun listAppsC(context: Context): List<String> {
+//        val appInfoList = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+//
+//        var string: List<String> = emptyList()
+//
+//        appInfoList.forEach {
+//            if (context.getPackageManager().getLaunchIntentForPackage(it.packageName) != null){
+//
+//                string = string.plus(it.loadLabel(packageManager).toString())
+//            }
+//
+//        }
+//        return string
+//
+//    }
 
-        var string: List<String> = emptyList()
+}
 
-        appInfoList.forEach {
-            if (context.getPackageManager().getLaunchIntentForPackage(it.packageName) != null){
-
-                string = string.plus(it.loadLabel(packageManager).toString())
-            }
-
+@Composable
+fun ListOfApps(context: Context, viewModelApps: ViewModelApps){
+    var list = viewModelApps.listAppsC(context = context)
+    
+    LazyColumn(){
+        items(list){
+            Text(text = it)
+            Divider()
         }
-        return string
-
     }
-
 }
 
 
--- app/src/main/java/com/acode/installedapps/viewModels/viewModel_Apps.kt
+++ app/src/main/java/com/acode/installedapps/viewModels/viewModel_Apps.kt
@@ -1,2 +1,26 @@
 package com.acode.installedapps.viewModels
 
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+
+class ViewModelApps() : ViewModel() {
+
+
+    fun listAppsC(context: Context): List<String> {
+        val pm = context.packageManager
+        val appInfoList = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+
+        var string = emptyList<String>()
+
+        appInfoList.forEach {
+            if (pm.getLaunchIntentForPackage(it.packageName) != null) {
+                string = string.plus(it.loadLabel(pm).toString())
+            }
+        }
+        return string
+    }
+}
