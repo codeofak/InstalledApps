18e368b5a94cd42cc6c4919c12e2aec44e588cfc
Abdul Kavi <abdkavi.github@gmail.com>
2022-07-23 13:07:14 +0530

    Added app icon an uninstall button

--- app/src/main/java/com/acode/installedapps/MainActivity.kt
+++ app/src/main/java/com/acode/installedapps/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.acode.installedapps
 
 import android.content.Context
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
+import coil.compose.AsyncImage
 import com.acode.installedapps.ui.theme.InstalledAppsTheme
-import com.acode.installedapps.viewModels.ViewModelApps
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
@@ -30,11 +49,11 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
 
-                    val viewModelApps: ViewModelApps by viewModels()
-
                     val context = LocalContext.current
+                    //val viewModelApps: ViewModelApps by viewModels()
 
-                    ListOfApps(context = context, viewModelApps = viewModelApps)
+
+                    ListOfApps(context = context)
 
                 }
             }
@@ -44,13 +63,62 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ListOfApps(context: Context, viewModelApps: ViewModelApps) {
-    val list = viewModelApps.listAppsC(context = context)
+fun ListOfApps(context: Context) {
 
-    LazyColumn {
-        items(list) {
-            Text(text = it)
-            Divider()
+    val pm = context.packageManager
+    val packages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+    val appPackages: MutableList<ApplicationInfo> = emptyList<ApplicationInfo>().toMutableList()
+
+
+    val coroutineScope = rememberCoroutineScope()
+
+
+
+    packages.forEach {
+        if (pm.getLaunchIntentForPackage(it.packageName) != null) {
+            appPackages += it
+        }
+    }
+
+    val intent = Intent(Intent.ACTION_DELETE)
+
+    Surface() {
+
+        LazyColumn {
+            items(appPackages){
+
+                Row(
+                    modifier = Modifier
+                        .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                        .fillMaxWidth()
+                        .clickable {
+                            coroutineScope.launch {
+                                context.startActivity(pm.getLaunchIntentForPackage(it.packageName))
+                            }
+                        },
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Box(modifier = Modifier
+                        .size(48.dp)
+                        .clip(shape = RoundedCornerShape(2.dp))){
+
+                        AsyncImage(model = it.loadIcon(pm), contentDescription = null)
+                    }
+
+                    Text(text = it.loadLabel(context.packageManager).toString())
+
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                context.startActivity(intent.setData(Uri.parse("package:${it.packageName}")))
+                            }
+
+                        }
+                    ) {
+                        Text(text = "Uninstall")
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/acode/installedapps/viewModels/viewModel_Apps.kt
+++ app/src/main/java/com/acode/installedapps/viewModels/viewModel_Apps.kt
@@ -1,24 +1,84 @@
 package com.acode.installedapps.viewModels
 
-import android.content.Context
+import android.app.Application
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
+import java.util.Collections.emptyList
+import android.content.Context as Context1
 
 
-class ViewModelApps : ViewModel() {
+class ViewModelApps(application: Application) : AndroidViewModel(application) {
 
 
-    fun listAppsC(context: Context): List<String> {
+
+    fun packageDetail(context: Context1): MutableList<ApplicationInfo> {
+        val pm = context.packageManager
+        val appInfoList = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+        return appInfoList
+    }
+
+
+    fun applicationLabel(context: Context1) {
+
+
+        val pm = context.packageManager
+        val packages = packageDetail(context)
+
+        var appLabel = emptyList<String>()
+        var appIcon = emptyList<Int>()
+
+        packages.forEach {
+            if (pm.getLaunchIntentForPackage(it.packageName) != null) {
+                appLabel = appLabel.plus(it.loadLabel(pm).toString())
+                appIcon = appIcon.plus(it.icon)
+            }
+        }
+//        return appLabel
+//        return appIcon
+    }
+
+
+    fun applicationIcon(context: Context1) {
+        val pm = context.packageManager
+        val packages = packageDetail(context = context)
+
+        var appIcon = ""
+
+        packages.forEach {
+            if (pm.getLaunchIntentForPackage(it.packageName) != null) {
+                appIcon = appIcon.plus(it.icon)
+            }
+        }
+    }
+
+    fun applicationPackages(context: Context1): List<ApplicationInfo> {
         val pm = context.packageManager
         val appInfoList = pm.getInstalledApplications(PackageManager.GET_META_DATA)
 
-        var string = emptyList<String>()
+        val appPackages = listOf<ApplicationInfo>()
+
 
         appInfoList.forEach {
             if (pm.getLaunchIntentForPackage(it.packageName) != null) {
-                string = string.plus(it.loadLabel(pm).toString())
+                appPackages.plus(it)
             }
         }
-        return string
+        return appPackages
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
