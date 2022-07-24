315ccce7f79e5cc222c64299322a6584ffeb2e8c
Abdul Kavi <abdkavi.github@gmail.com>
2022-07-24 11:36:16 +0530

    using coroutines ..

--- app/src/main/java/com/acode/installedapps/MainActivity.kt
+++ app/src/main/java/com/acode/installedapps/MainActivity.kt
@@ -9,34 +9,26 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.paging.compose.LazyPagingItems
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import com.acode.installedapps.ui.theme.InstalledAppsTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
+import com.acode.installedapps.viewModels.ViewModelApps
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -50,10 +42,10 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background) {
 
                     val context = LocalContext.current
-                    //val viewModelApps: ViewModelApps by viewModels()
+                    val viewModelApps: ViewModelApps by viewModels()
 
 
-                    ListOfApps(context = context)
+                    ListOfApps(context = context,viewModelApps = viewModelApps)
 
                 }
             }
@@ -63,29 +55,29 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ListOfApps(context: Context) {
+fun ListOfApps(context: Context,viewModelApps: ViewModelApps) {
 
     val pm = context.packageManager
-    val packages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
-    val appPackages: MutableList<ApplicationInfo> = emptyList<ApplicationInfo>().toMutableList()
+//    val packages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+//    val appPackages: MutableList<ApplicationInfo> = emptyList<ApplicationInfo>().toMutableList()
 
+    val appPackages = viewModelApps.loadAppPackages(context)
 
     val coroutineScope = rememberCoroutineScope()
 
 
-
-    packages.forEach {
-        if (pm.getLaunchIntentForPackage(it.packageName) != null) {
-            appPackages += it
-        }
-    }
+//    packages.forEach {
+//        if (pm.getLaunchIntentForPackage(it.packageName) != null) {
+//            appPackages += it
+//        }
+//    }
 
     val intent = Intent(Intent.ACTION_DELETE)
 
-    Surface() {
+    Surface {
 
         LazyColumn {
-            items(appPackages){
+            items(appPackages) {
 
                 Row(
                     modifier = Modifier
@@ -98,12 +90,15 @@ fun ListOfApps(context: Context) {
                         },
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Box(modifier = Modifier
-                        .size(48.dp)
-                        .clip(shape = RoundedCornerShape(2.dp))){
 
-                        AsyncImage(model = it.loadIcon(pm), contentDescription = null)
-                    }
+                    //Icon
+                    //AsyncImage(model = it.loadIcon(pm), contentDescription = null)   ---> Don't use it directly
+                    Image(
+                        modifier = Modifier.size(48.dp),
+                        painter = rememberAsyncImagePainter(model = it.loadIcon(pm),
+                            filterQuality = FilterQuality.Low),
+                        contentDescription = "App Icon"
+                    )
 
                     Text(text = it.loadLabel(context.packageManager).toString())
 
--- app/src/main/java/com/acode/installedapps/viewModels/viewModel_Apps.kt
+++ app/src/main/java/com/acode/installedapps/viewModels/viewModel_Apps.kt
@@ -1,72 +1,36 @@
 package com.acode.installedapps.viewModels
 
 import android.app.Application
+import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import androidx.lifecycle.AndroidViewModel
-import java.util.Collections.emptyList
-import android.content.Context as Context1
 
 
 class ViewModelApps(application: Application) : AndroidViewModel(application) {
 
+    private val context = getApplication<Application>()
 
-
-    fun packageDetail(context: Context1): MutableList<ApplicationInfo> {
-        val pm = context.packageManager
-        val appInfoList = pm.getInstalledApplications(PackageManager.GET_META_DATA)
-        return appInfoList
+    init {
+        loadAppPackages(context = context)
     }
 
-
-    fun applicationLabel(context: Context1) {
-
-
+    fun loadAppPackages(context: Context): MutableList<ApplicationInfo> {
         val pm = context.packageManager
-        val packages = packageDetail(context)
-
-        var appLabel = emptyList<String>()
-        var appIcon = emptyList<Int>()
+        val packages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+        val appPackages: MutableList<ApplicationInfo> =
+            kotlin.collections.emptyList<ApplicationInfo>()
+                .toMutableList()
 
         packages.forEach {
             if (pm.getLaunchIntentForPackage(it.packageName) != null) {
-                appLabel = appLabel.plus(it.loadLabel(pm).toString())
-                appIcon = appIcon.plus(it.icon)
+                appPackages += it
             }
         }
-//        return appLabel
-//        return appIcon
-    }
 
-
-    fun applicationIcon(context: Context1) {
-        val pm = context.packageManager
-        val packages = packageDetail(context = context)
-
-        var appIcon = ""
-
-        packages.forEach {
-            if (pm.getLaunchIntentForPackage(it.packageName) != null) {
-                appIcon = appIcon.plus(it.icon)
-            }
-        }
-    }
-
-    fun applicationPackages(context: Context1): List<ApplicationInfo> {
-        val pm = context.packageManager
-        val appInfoList = pm.getInstalledApplications(PackageManager.GET_META_DATA)
-
-        val appPackages = listOf<ApplicationInfo>()
-
-
-        appInfoList.forEach {
-            if (pm.getLaunchIntentForPackage(it.packageName) != null) {
-                appPackages.plus(it)
-            }
-        }
         return appPackages
-
     }
+
 }
 
 
